4b9164aa894d36d790e1ad9bcd5a1e0557c35289
Miguel Valencia <valencia.miguel.41@gmail.com>
2024-04-29 02:40:42 -0500

    loads of changes. Finished formating most of the pages

--- pages/tables_view/tablas.js
+++ pages/tables_view/tablas.js
@@ -1,22 +1,39 @@
-function addDataToTable(){
+async function retrieveUserData(){
 
-    const table = document.getElementById('user-table').getElementsByTagName('tbody')[0];
+    let users;
 
-    const newRow = table.insertRow();
-    const cell1 = newRow.insertCell(0);
-    const cell2 = newRow.insertCell(1);
-    const cell3 = newRow.insertCell(2);
-    const cell4 = newRow.insertCell(3);
+    try{
+        const response = await fetch('https://dummyjson.com/users?limit=20&select=firstName,lastName,age,phone,email');
 
-    cell1.innerHTML = User.name;
-    cell2.innerHTML = User.age;
-    cell3.innerHTML = User.telephone;
-    cell4.innerHTML = User.email;
+        responseData = await response.json();
+
+        users = responseData.users;
+    }
+    catch(error){
+        console.log(`Failed to retrieve user data: ${error}`);
+    }
+
+    for(let user of users){
+        assignUserDataToTable(user);
+    }
 }
 
-const User ={
-    name: 'user',
-    age: 36,
-    telephone: 12333,
-    email: 'user@example.com'
+function assignUserDataToTable(user){
+    const  {firstName, lastName, age, email, phone} = user;
+    const tableBody = document.getElementById('user-table').getElementsByTagName('tbody')[0];
+
+    const fullName = `${firstName} ${lastName}`
+
+    const row = tableBody.insertRow();
+    const nameCell = row.insertCell(0);
+    const ageCell = row.insertCell(1);
+    const phoneCell = row.insertCell(2);
+    const emailCell = row.insertCell(3);
+
+    nameCell.textContent = fullName;
+    ageCell.textContent = age;
+    phoneCell.textContent = phone;
+    emailCell.textContent = email;
 }
+
+retrieveUserData();
